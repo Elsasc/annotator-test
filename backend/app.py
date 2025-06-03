096d16d374b19e5b1cebab11e7a3ae477b1bc4f6
hitanshu-p <hitanshu88@outlook.com>
2025-06-03 14:48:48 -0700

    changed port to link with backend

--- backend/app.py
+++ backend/app.py
@@ -36,4 +36,5 @@ def upload():
     return send_file(output_path, as_attachment=True, download_name=output_filename)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
